a627c5b405ef8e95c369aa7c0396ed1b5dacfd15
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-04-21 13:46:47 -0300

    bankslip search

---
+++ src/Controllers/Finance/BankSlipControllerSection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Sufficit.Finance;
+using Sufficit.Identity;
+using Sufficit.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sufficit.Client.Controllers
+{
+    public sealed class BankSlipControllerSection : AuthenticatedControllerSection, IBankSlipController
+    {
+        private const string Controller = FinanceControllerSection.Controller;
+        private const string Prefix = "/bankslip";
+
+        private readonly JsonSerializerOptions _json;
+
+        public BankSlipControllerSection(IAuthenticatedControllerBase cb) : base(cb)
+        {
+            _json = cb.Json;
+        }
+
+        ////// <inheritdoc cref="IMessagesController.GetDetails(MessageDetailsSearchParameters, CancellationToken) "/>
+        [Authorize(Roles = $"{FinancialManagerRole.NormalizedName},{FinancialRole.NormalizedName},{ManagerRole.NormalizedName},{AdministratorRole.NormalizedName}")]
+        public Task<IEnumerable<BankSlipInfo>> Search(BankSlipSearchParameters parameters, CancellationToken cancellationToken)
+        {
+            var uri = new Uri($"{Controller}{Prefix}/search", UriKind.Relative);
+            var content = JsonContent.Create(parameters, null, _json);
+            var message = new HttpRequestMessage(HttpMethod.Post, uri)
+            {
+                Content = content
+            };
+            return RequestMany<BankSlipInfo>(message, cancellationToken);
+        }
+    }
+}
---
+++ src/Controllers/Finance/FinanceControllerSection.cs
@@ -0,0 +1,16 @@
+﻿using Sufficit.Net.Http;
+
+namespace Sufficit.Client.Controllers
+{
+    public sealed class FinanceControllerSection : AuthenticatedControllerSection
+    {
+        public const string Controller = "/finance";
+
+        public FinanceControllerSection(IAuthenticatedControllerBase cb) : base(cb)
+        {
+            BankSlip = new BankSlipControllerSection(cb);
+        }
+
+        public BankSlipControllerSection BankSlip { get; }
+    }
+}
