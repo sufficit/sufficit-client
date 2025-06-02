c9e39a3b0d9afa19dfbe490027e91126ba1da44c
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-02 13:53:20 -0300

    Refatora validação de contato e simplifica API
    Atualiza `NotificationContactValidate.razor.cs` para usar a resposta da API diretamente, melhorando a clareza do código.
    
    Modifica `NotificationContactControllerSection.cs` para alterar a assinatura do método `Validate`, retornando um `Task<EndPointResponse<ContactValidationResponse>>`. A lógica de manipulação de erros foi removida, centralizando o tratamento de respostas em um único método.

--- src/Controllers/Notification/NotificationContactControllerSection.cs
+++ src/Controllers/Notification/NotificationContactControllerSection.cs
@@ -23,52 +23,12 @@ namespace Sufficit.Client.Controllers.Notification
             _json = cb.Json;
         }
 
-        public async Task<ContactValidationResponse> Validate(ContactValidationRequest parameters, CancellationToken cancellationToken = default)
-        {
-            var result = new ContactValidationResponse() 
-            { 
-                Success = false,
-                Channel = parameters.Channel,
-                Destination = parameters.Destination,
-            };           
-
+        public Task<EndPointResponse<ContactValidationResponse>> Validate(ContactValidationRequest parameters, CancellationToken cancellationToken = default)
+        {       
             var uri = new Uri($"{Controller}{Prefix}/validate", UriKind.Relative);
             var message = new HttpRequestMessage(HttpMethod.Post, uri);
             message.Content = JsonContent.Create(parameters, null, _json);
-
-            using var response = await SendAsync(message, cancellationToken);
-            try
-            {
-                await response.EnsureSuccess(cancellationToken);
-                result.Success = true;
-            } 
-            catch (HttpRequestException ex)
-            {
-                try
-                {
-                    var epresp = JsonSerializer.Deserialize<EndPointResponse>(ex.Message);
-                    if (epresp != null)
-                    {
-                        result.Success = epresp.Success;
-                        result.Message = epresp.Message;
-                        result.Link = epresp.Link;
-                        result.Data = epresp.Data;
-                        return result;
-                    }
-                }
-                catch { }
-            }
-
-            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
-                return result;
-
-            if (response.Content.Headers.ContentLength > 0 && response.Content.Headers.ContentType?.MediaType == "application/json")
-            {
-                var response_object = await response.Content.ReadFromJsonAsync<ContactValidationResponse>(_json, cancellationToken);
-                if (response_object != null) result = response_object;
-            }
-
-            return result;
+            return Request<EndPointResponse<ContactValidationResponse>>(message, cancellationToken)!;
         }
 
         protected override string[]? AnonymousPaths { get; } = { $"{Controller}{Prefix}/validate" };
